961d84fbd533c22fbe0e68ead05e0b80fc7bd4e6
Georgi Georgiev <gardax95@gmail.com>
2015-02-28 14:44:54 +0200

    mqtt package and simple example added

--- client/app/controllers/DevCtrl.js
+++ client/app/controllers/DevCtrl.js
@@ -4,7 +4,27 @@
 angular.module('dsh.controllers').
   controller('DevCtrl', ['$rootScope', '$meteor', function ($rootScope, $meteor) {
     var devCtrl = this;
+    var mqttClient = mqtt.connect('ws://test.mosquitto.org:8080', {
+      clientId: "fromServer"
+    });
+    mqttClient.on("connect", function() {
+      console.log("Connected");
 
-    devCtrl.hello = 'Hello world!';
+      mqttClient.on("message", function(param, p2) {
+        console.log("Server message", param, p2);
+      });
+
+      mqttClient.on("error", function(param) {
+        console.log("Wahhhh");
+      });
+
+      mqttClient.subscribe("test/topic");
+
+      mqttClient.publish("test/topic", "hello world", function() {
+        console.log("Sent Sir!");
+      });
+
+
+    });
 
   }]);
